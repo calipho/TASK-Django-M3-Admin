50f19fd8af020f59dbb3ebefe2a67ad0a0e2a86b
calipho <106757959+calipho@users.noreply.github.com>
2022-07-18 20:44:30 +0300

    done

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Pokemon
 
-# Register your models here.
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'hp', 'active',
+                    )
+    list_filter = ('type', 'active')
+    search_fields = ('name', 'name_fr', 'name_ar', 'name_jp')
+    ordering = ('name',)
+    date_hierarchy = 'created_at'
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'type', 'hp', 'active')
+        }),
+        ('Localizations', {
+            'classes': ('collapse',),
+            'fields': ('name_fr', 'name_ar', 'name_jp')
+        }),
+        ('Dates', {
+            'classes': ('collapse',),
+            'fields': ('created_at', 'modified_at')
+        })
+    )
+    readonly_fields = ('created_at', 'modified_at')
+    save_on_top = True
+    save_as = True
+    inlines = []
+    filter_horizontal = ()
+    prepopulated_fields = {}
+    radio_fields = {}
+    list_editable = ('active',)
+    list_per_page = 25
+    actions_on_top = True
+    actions_on_bottom = True
+    actions_selection_counter = True
+    # ...
+
+    # Register your models here.
+admin.site.register(Pokemon, PokemonAdmin)
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30)
+
+    class PokemonType(models.TextChoices):
+
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+
+    type = models.CharField(
+        max_length=2, choices=PokemonType.choices, default=PokemonType.WATER)
+    hp = models.IntegerField(
+        default=50, validators=[
+            MaxValueValidator(350),
+            MinValueValidator(50)
+        ])
+    active = models.BooleanField(default=True)
+    name_fr = models.CharField(max_length=30, default='', blank=True)
+    name_ar = models.CharField(max_length=30, default='', blank=True)
+    name_jp = models.CharField(max_length=30, default='', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
